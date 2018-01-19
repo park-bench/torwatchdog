ae1d8ba222b9df34dacb0614d65a10e3f4709954
eviljoel <eviljoel@linux.com>
2018-01-18 19:39:01 -0600

    Removed references to verify_string_exists_prelogging.

--- src/usr/share/torwatchdog/torwatchdog.py
+++ src/usr/share/torwatchdog/torwatchdog.py
@@ -84,8 +84,8 @@ config_file.read('/etc/torwatchdog/torwatchdog.conf')
 
 # Logging config goes first
 config_helper = confighelper.ConfigHelper()
-log_file = config_helper.verify_string_exists_prelogging(config_file, 'log_file')
-log_level = config_helper.verify_string_exists_prelogging(config_file, 'log_level')
+log_file = config_helper.verify_string_exists(config_file, 'log_file')
+log_level = config_helper.verify_string_exists(config_file, 'log_level')
 
 config_helper.configure_logger(log_file, log_level)
 
